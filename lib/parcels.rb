a26e0468803283c330ba729b13c58de562780233
Anna Marie Cooper and Bryce Frazier <bryce@>
2018-02-12 13:53:12 -0800

    first meaningful fail

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,5 +1,12 @@
-class Name
-  def
+class Parcel
+  def initialize(width, length, height)
+    @width = width
+    @length = length
+    @height = height
+  end
+
+  def volume_calculator
+
   end
 
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -1,3 +1,10 @@
 require('parcels')
 require('pry')
 require('rspec')
+
+describe('#volume_calculator') do
+  it("calculates volume based on width, length and height and returns the value") do
+    volume = Parcel.new(2, 2, 4)
+    expect(volume.volume_calculator()).to(eq(16))
+  end
+end
