de64ea83c4f0132d57227085d5dfc5da6b77b512
Anna Marie Cooper and Bryce Frazier <anna@>
2018-02-12 16:07:18 -0800

    pass all tests

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,12 +1,51 @@
 class Parcel
-  def initialize(width, length, height)
+  def initialize(width, length, height, weight, speed, distance)
     @width = width
     @length = length
     @height = height
+    @weight = weight
+    @speed = speed
+    @distance = distance
   end
 
-  def volume_calculator
-    @volume = @width * @length * @height
+  def volume()
+    volume = @width * @length * @height
+    if (volume >= 500)
+      @volume_cost = 5
+    else
+      @volume_cost = 0
+    end
   end
 
+  def weight()
+    if (@weight <= 5)
+      @weight_cost = 5
+    else
+      @weight_cost = 10
+    end
+  end
+
+  def speed()
+    if @speed == 5
+      @speed_cost = 5
+    else
+      @speed_cost = 0
+    end
+  end
+
+  def distance()
+    if @distance == 1
+      @distance_cost = 5
+    else
+      @distance_cost = 0
+    end
+  end
+
+  def cost_to_ship()
+    self.weight()
+    self.distance()
+    self.speed()
+    self.volume()
+    cost = (@distance_cost + @speed_cost + @volume_cost + @weight_cost)
+  end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,9 +2,40 @@ require('parcels')
 require('pry')
 require('rspec')
 
-describe('#volume_calculator') do
-  it("calculates volume based on width, length and height and returns the value") do
-    volume = Parcel.new(2, 2, 4)
-    expect(volume.volume_calculator()).to(eq(16))
-  end
+  describe('#parcels') do
+    describe('#volume') do
+      it("calculates volume based on width, length and height and returns the value") do
+        volume = Parcel.new(2, 2, 4, 0, 0, 0)
+        expect(volume.volume()).to(eq(0))
+      end
+    end
+
+    describe('#weight') do
+      it("returns $5 for parcels less than 5lbs") do
+        weight = Parcel.new(2, 2, 4, 3, 0, 0)
+        expect(weight.weight()).to(eq(5))
+      end
+    end
+
+    describe('#speed') do
+      it("returns $5 for a parcel over 500 unit of volume") do
+        speed = Parcel.new(10, 10, 6, 10, 5, 0)
+        expect(speed.speed()).to(eq(5))
+      end
+    end
+
+    describe('#distance') do
+      it("returns $5 for a long distance parcel") do
+        distance = Parcel.new(10, 10, 6, 10, 5, 1)
+        expect(distance.distance()).to(eq(5))
+      end
+    end
+
+    describe('#cost_to_ship') do
+      it("returns $5 for a long distance parcel") do
+        cost = Parcel.new(10, 10, 6, 10, 5, 1)
+        expect(cost.cost_to_ship()).to(eq(25))
+      end
+    end
+
 end
